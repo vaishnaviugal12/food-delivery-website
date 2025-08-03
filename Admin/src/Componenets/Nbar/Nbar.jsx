78c43583e980ac5e88439bbd9fd99117a8ace0a5
Vaishnaviugale <149070183+vaishnaviugal12@users.noreply.github.com>
2025-08-03 20:34:37 +0530

    Update Nbar.jsx

--- Admin/src/Componenets/Nbar/Nbar.jsx
+++ Admin/src/Componenets/Nbar/Nbar.jsx
@@ -11,7 +11,7 @@ const Nbar = () => {
   return (
     <div className='navbar'>
         <img className='logo' src={assets.logo}></img>
-        <img  className='profile' src={assets.profile_image}></img>
+        <img onclick={logout} className='profile' src={assets.profile_image}></img>
     </div>
   )
 }
