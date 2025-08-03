f4df01d293c8041cf351f6950efd6be293e86f8b
vaishnvai ugale <vaishnvai ugale>
2025-08-03 14:45:23 +0530

    Changes in Readme.md file

--- Bakend/config/db.js
+++ Bakend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async ()=>{
-  await mongoose.connect('mongodb+srv://vaishnaviugale14:9322473414@cluster0.rcr2yeo.mongodb.net/food-del').then(()=>console.log("db connected"))
+  await mongoose.connect(process.env.MONGPDB_URL).then(()=>console.log("db connected"))
 }
